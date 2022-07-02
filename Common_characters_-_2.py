7cdb89a04a8a48c6b6455d80b4494aa808cd0cca
sasipriyanka <98680826+sasipriyanka2121@users.noreply.github.com>
2022-07-02 21:25:18 +0530

    s=input().lower() l=s.split() l1=list(l[0]) l.remove(l[0]) c=0 e=[] for i in l1:     for j in range(len(l)):         if i not in l[j] and i not in e:             break     else:        e.append(i) print(len(e))

---
+++ Common_characters_-_2.py
@@ -0,0 +1,13 @@
+s=input().lower()
+l=s.split()
+l1=list(l[0])
+l.remove(l[0])
+c=0
+e=[]
+for i in l1:
+    for j in range(len(l)):
+        if i not in l[j] and i not in e:
+            break
+    else:
+       e.append(i)
+print(len(e))        
